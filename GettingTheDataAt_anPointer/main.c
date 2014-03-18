5d9711357a7c34ce63bc97c2eb97fd12ea5090b6
ammar falmban <devAspirant@gmail.com>
2014-03-18 15:26:42 +0900

    Understanding how to get a data from a pointer

--- GettingTheDataAt_anPointer/main.c
+++ GettingTheDataAt_anPointer/main.c
@@ -11,8 +11,18 @@
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    int i = 17;
+    int *addressOfI = &i;
+    // ①
+    /* The first is in the declaration where you declare the variable addressOfI to be an int *. That is, it is a pointer to a place where an int can be stored.*/
+    printf(" I Stores its value at %p \n",addressOfI);
+    printf(" The int stored at addressIO : %d \n",*addressOfI);
+
+    //
+    /* The second is where you read the int value that is stored at the address stored in addressOfI. (Pointers are also called references. Thus, using the pointer to read data at the address is sometimes called dereferencing the pointer.) */
+    
+    *addressOfI = 89;
+    printf(" Now i is : %d\n",i);
     return 0;
 }
 
